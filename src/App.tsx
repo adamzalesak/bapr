0de580d4cf4024a706339577f1f7c5ea8c0b90eb
Adam Zálešák <adam.zalesak@notino.com>
2022-12-02 16:20:37 +0100

    feat: root styles refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,8 @@
-import { createTheme, ThemeProvider, styled } from '@mui/material/styles';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { RecoilRoot } from 'recoil';
 import { Main } from './components/Main';
 
 import './i18n/i18n';
-
-const AppWrapper = styled('div')`
-  height: 100vh;
-  width: 100vw;
-`;
 
 const theme = createTheme({
   typography: { fontFamily: 'Roboto, Inter, Avenir, Helvetica, Arial, sans-serif' },
@@ -18,11 +13,11 @@ const theme = createTheme({
     success: {
       main: '#82cc88',
     },
-    error: {
-      main: '#c7677d',
-    },
     warning: {
       main: '#eee275',
+    },
+    error: {
+      main: '#c7677d',
     },
   },
 });
@@ -31,9 +26,7 @@ export const App = () => {
   return (
     <RecoilRoot>
       <ThemeProvider theme={theme}>
-        <AppWrapper>
-          <Main />
-        </AppWrapper>
+        <Main />
       </ThemeProvider>
     </RecoilRoot>
   );
